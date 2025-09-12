936ad4dd2b557661f4b840ecfc1fe4f9740c9677
pauliiortizz <127853900+pauliiortizz@users.noreply.github.com>
2025-09-12 11:24:08 -0300

    Update Light class constructor and add light_matrix
    Refactor Light class to include direction and light matrix property.

--- TP4/light.py
+++ TP4/light.py
@@ -1,5 +1,12 @@
+import glm 
+
 class Light:
-    def __init__(self):
-        self.position = [0, 0, 0]
-        self.color = [1, 1, 1]
+    def __init__(self, position = (0.0,5.0,-10.0), color = (1.0,1.0,1.0), direction = (0.0,0.0,0.0)):
+        self.position = position
+        self.color = color
+        self.direction = direction
         self.intensity = 1.0
+
+    @property
+    def light_matrix(self):
+        return glm.lookAt(self.position, (0.0,0.1,0.0), self.direction)
